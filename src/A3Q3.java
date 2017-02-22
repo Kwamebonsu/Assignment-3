dd95537c6ec84bb1fcd75fa47729519f5a7b12be
bonsk5852 <bonsk5852@HRH-IC0027340.wrdsb.ca>
2017-02-22 10:15:11 -0500

    Initial Commit

--- src/A3Q3.java
+++ src/A3Q3.java
@@ -1,3 +1,10 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Thing;
+import becker.robots.Wall;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -14,5 +21,46 @@ public class A3Q3 {
      */
     public static void main(String[] args) {
         // create a city
+        City kw = new City();
+        
+        //create the walls
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        new Wall(kw, 1, 3, Direction.NORTH);
+        new Wall(kw, 1, 4, Direction.NORTH);
+        
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        new Wall(kw, 3, 1, Direction.WEST);
+        
+        new Wall(kw, 1, 4, Direction.EAST);
+        new Wall(kw, 2, 4, Direction.EAST);
+        new Wall(kw, 3, 4, Direction.EAST);
+        
+        new Wall(kw, 3, 1, Direction.SOUTH);
+        new Wall(kw, 3, 2, Direction.SOUTH);
+        new Wall(kw, 3, 3, Direction.SOUTH);
+        new Wall(kw, 3, 4, Direction.SOUTH);
+        
+        //create things
+        new Thing(kw, 1, 2);
+        new Thing(kw, 2, 2);
+        new Thing(kw, 2, 3);
+        new Thing(kw, 2, 4);
+        new Thing(kw, 3, 1);
+        new Thing(kw, 3, 4);
+        
+        Robot karel = new Robot(kw, 1, 1, Direction.EAST);
+        
+        while(true){
+            while(karel.canPickThing()){
+                karel.pickThing();
+            }
+            while(karel.frontIsClear()){
+            karel.move();
+            }
+            }
+        }
+        
     }
 }
