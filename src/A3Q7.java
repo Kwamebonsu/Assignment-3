1e478fdca1242ec6ee40ad88b7f0719364b98c8f
bonsk5852 <bonsk5852@HRH-IC0027340.wrdsb.ca>
2017-02-27 10:15:14 -0500

    Initial Commit

---
+++ src/A3Q7.java
@@ -0,0 +1,82 @@
+
+import becker.robots.City;
+import becker.robots.Direction;
+import becker.robots.Robot;
+import becker.robots.Wall;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author bonsk5852
+ */
+public class A3Q7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // create a city
+        City kw = new City();
+        
+        //create square one
+        new Wall(kw, 1, 1, Direction.NORTH);
+        new Wall(kw, 1, 2, Direction.NORTH);
+        
+        new Wall(kw, 1, 1, Direction.WEST);
+        new Wall(kw, 2, 1, Direction.WEST);
+        
+        new Wall(kw, 2, 1, Direction.SOUTH);
+        new Wall(kw, 2, 2, Direction.SOUTH);
+        
+        new Wall(kw, 1, 2, Direction.EAST);
+        new Wall(kw, 2, 2, Direction.EAST);
+        
+         //create square two
+        new Wall(kw, 1, 4, Direction.NORTH);
+        new Wall(kw, 1, 5, Direction.NORTH);
+        
+        new Wall(kw, 1, 4, Direction.WEST);
+        new Wall(kw, 2, 4, Direction.WEST);
+        
+        new Wall(kw, 2, 4, Direction.SOUTH);
+        new Wall(kw, 2, 5, Direction.SOUTH);
+        
+        new Wall(kw, 1, 5, Direction.EAST);
+        new Wall(kw, 2, 5, Direction.EAST);
+        
+        //create square three
+        new Wall(kw, 4, 1, Direction.NORTH);
+        new Wall(kw, 4, 2, Direction.NORTH);
+        
+        new Wall(kw, 4, 1, Direction.WEST);
+        new Wall(kw, 5, 1, Direction.WEST);
+        
+        new Wall(kw, 5, 1, Direction.SOUTH);
+        new Wall(kw, 5, 2, Direction.SOUTH);
+        
+        new Wall(kw, 5, 2, Direction.EAST);
+        new Wall(kw, 4, 2, Direction.EAST);
+        
+       //create square four
+        new Wall(kw, 4, 4, Direction.NORTH);
+        new Wall(kw, 4, 5, Direction.NORTH);
+        
+        new Wall(kw, 4, 4, Direction.WEST);
+        new Wall(kw, 5, 4, Direction.WEST);
+        
+        new Wall(kw, 5, 4, Direction.SOUTH);
+        new Wall(kw, 5, 5, Direction.SOUTH);
+        
+        new Wall(kw, 5, 5, Direction.EAST);
+        new Wall(kw, 4, 5, Direction.EAST);
+        
+        Robot karel = new Robot(kw, 3, 3, Direction.SOUTH);
+        
+        while(true){
+        }
+    }
+}
